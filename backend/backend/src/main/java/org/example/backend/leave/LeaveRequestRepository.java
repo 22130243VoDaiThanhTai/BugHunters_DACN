5c19d5d5652e96cddd5bd466e53860e64631db54
ThoaiImposter <22130273@st.hcmuaf.edu.vn>
2026-04-10 08:38:41 +0700

    be history nhan vien, sua conflict

--- backend/backend/src/main/java/org/example/backend/leave/LeaveRequestRepository.java
+++ backend/backend/src/main/java/org/example/backend/leave/LeaveRequestRepository.java
@@ -12,20 +12,20 @@ public interface LeaveRequestRepository extends JpaRepository<LeaveRequest, Long
 
     long countByStatus(LeaveStatus status);
 
-<<<<<<< HEAD
-        @Query("""
-                        select coalesce(sum(lr.totalDays), 0)
-                        from LeaveRequest lr
-                        where lr.userId = :userId
-                            and lr.status = :status
-                            and lr.startDate between :fromDate and :toDate
-                        """)
-        Integer sumTotalDaysByUserIdAndStatusAndStartDateBetween(
-                        @Param("userId") Long userId,
-                        @Param("status") LeaveStatus status,
-                        @Param("fromDate") LocalDate fromDate,
-                        @Param("toDate") LocalDate toDate);
-=======
+    @Query("""
+        select coalesce(sum(lr.totalDays), 0)
+        from LeaveRequest lr
+        where lr.userId = :userId
+            and lr.status = :status
+            and lr.startDate between :fromDate and :toDate
+        """)
+    Integer sumTotalDaysByUserIdAndStatusAndStartDateBetween(
+        @Param("userId") Long userId,
+        @Param("status") LeaveStatus status,
+        @Param("fromDate") LocalDate fromDate,
+        @Param("toDate") LocalDate toDate);
+
     List<LeaveRequest> findByStatusOrderByCreatedAtDesc(LeaveStatus status);
->>>>>>> bdedd7c (View Pending request and View Personal Leave Requset History)
+
+    List<LeaveRequest> findByUserIdOrderByCreatedAtDesc(Long userId);
 }
